fb1a3f70fb946a53f4c66ecddce358ba1cad3dbb
Muhammad Umar <umarkohati01@gmail.com>
2026-08-13 00:37:29 +0500

    Restore FeatureReveal and ShowcaseStack sections, move WhyZynostSwap after Hero with CardSwap layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Hero } from "@/components/sections/Hero";
 import { TrustStrip } from "@/components/sections/TrustStrip";
 import { ProductExperience } from "@/components/sections/ProductExperience";
+import { FeatureReveal } from "@/components/sections/FeatureReveal";
 import { IntelligenceArchitecture } from "@/components/sections/IntelligenceArchitecture";
 import { WhyZynostSwap } from "@/components/sections/WhyZynostSwap";
 import { AgentsShowcase } from "@/components/sections/AgentsShowcase";
 import { SystemPlannedTrade } from "@/components/sections/SystemPlannedTrade";
 import { SkepticJudge } from "@/components/sections/SkepticJudge";
 import { BacktestTransparency } from "@/components/sections/BacktestTransparency";
+import { ShowcaseStack } from "@/components/sections/ShowcaseStack";
 import { Pricing } from "@/components/sections/Pricing";
 import { FAQ } from "@/components/sections/FAQ";
 import { FinalCTA } from "@/components/sections/FinalCTA";
@@ -15,14 +17,16 @@ export default function Home() {
   return (
     <>
       <Hero />
+      <WhyZynostSwap />
       <TrustStrip />
       <ProductExperience />
+      <FeatureReveal />
       <IntelligenceArchitecture />
-      <WhyZynostSwap />
       <AgentsShowcase />
       <SystemPlannedTrade />
       <SkepticJudge />
       <BacktestTransparency />
+      <ShowcaseStack />
       <Pricing />
       <FAQ />
       <FinalCTA />
